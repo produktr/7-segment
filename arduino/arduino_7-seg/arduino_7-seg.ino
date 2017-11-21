4cb71c712c4d4b5c6aaa1caecc734089cd174755
ProduktR <produktr@users.noreply.github.com>
2017-11-21 16:38:12 +0100

    Analog pins now digital, LED for rev warning

--- arduino/arduino_7-seg/arduino_7-seg.ino
+++ arduino/arduino_7-seg/arduino_7-seg.ino
@@ -3,6 +3,8 @@ int gear;
 int shift;
 int z;
 int selchar;
+int analog_out[5] = {A1, A2, A3, A4, A5};
+int analog_in[1] = {A0};
 int seg[8] = {2,3,4,5,6,7,8,9};
 int com[4] = {10,11,12,13};
 int cha[11][8] = {
@@ -15,18 +17,26 @@ int cha[11][8] = {
   {1,0,1,1,1,1,1,0}, // 6
   {0,0,0,0,0,0,1,0}, // -
   {0,0,0,0,0,0,1,0}, // -
-  {1,0,1,1,0,1,1,0}, // s (shift!)
+  {0,0,0,0,0,0,1,0}, // -
   {1,0,0,0,1,1,0,0}  // r
 };
 
 void setup() {
   Serial.begin(115200);
-   while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
+   while(!Serial){
+    ; // wait for serial port to connect
   }
   // pinMode pins 2-13 to OUTPUT
-  for(int thisPin = 2; thisPin < 14; thisPin++) {
+  for(int thisPin = 2; thisPin < 14; thisPin++){
     pinMode(thisPin, OUTPUT);
+  }
+  // pinMode pins A1-A5 to OUTPUT
+  for(int thisPin = 0; thisPin < 5; thisPin++){
+   pinMode(analog_out[thisPin], OUTPUT); 
+  }
+  // pinMode pin A0 to INPUT
+  for(int thisPin = 0; thisPin < 1; thisPin++){
+   pinMode(analog_in[thisPin], INPUT); 
   }
   // digitalWrite pins 2-9 set HIGH
   for(int thisPin = 2; thisPin < 10; thisPin++){
@@ -41,8 +51,11 @@ void setup() {
 void loop(){
   if(Serial.available() > 0){
     Serial.readBytes(bufferArray, 4);
-    setGear(bufferArray[0]);
-//    setGear(1);
+    if(digitalRead(analog_in[0]) === HIGH){
+      setSpeed(bufferArray[4]);
+    }else{
+      setGear(bufferArray[0]);
+    }
     setRevLight(bufferArray[2]);
   }
 }
@@ -64,7 +77,13 @@ void setSegments(int selchar){
   }
 }
 
-/* disply selected gear
+/* display speed of vehicle
+ */
+void setSpeed(int speed){
+  // code to set speed
+}
+
+/* display selected gear
  */
 void setGear(int gear){
   delay(10);
@@ -73,19 +92,14 @@ void setGear(int gear){
   digitalWrite(com[3], HIGH);
   delay(10);
   setSegments(gear);
-//  digitalWrite(com[3], LOW);
 }
 
-/* set shifter warning 'light'
+/* rev warning light
  */
 void setRevLight(int shift){
   if(shift > 0){
-    delay(10);
-    digitalWrite(com[3], LOW);
-    delay(10);
-    digitalWrite(com[0], HIGH);
-    delay(10);
-    setSegments(9);
-//    digitalWrite(com[0], LOW);
+    digitalWrite(analog_out[0], LOW);
+  }else{
+    digitalWrite(analog_out[0], HIGH);
   }
 }
